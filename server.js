6c4a8591d4f2b3280b711b280968b9f2ee29e2b4
codesforjenuel <codesforjenuel@gmail.com>
2023-04-19 20:39:39 -0700

    Port update to 8080

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./controllers')
 const express = require("express");
 const sequelize = require('./config/connection')
 const expbs = require("express-handlebars");
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8080
 const app = express();
 
 app.engine("handlebars", expbs.engine({ defaultLayout: "main" }));
